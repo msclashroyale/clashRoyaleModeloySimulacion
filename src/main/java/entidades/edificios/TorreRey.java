0c1f0b0cbac1a73842f8d4ad4c349377096ce1f5
Gonzalo Barroso <gonzalobarroso50@gmail.com>
2025-10-30 22:58:02 -0300

    Correccion: Torre rey puede atacar

--- src/main/java/entidades/edificios/TorreRey.java
+++ src/main/java/entidades/edificios/TorreRey.java
@@ -56,6 +56,7 @@ public class TorreRey extends Torre {
     public int atacar(Tropa objetivo, int tickActual) {
         if (puedeAtacar(tickActual)) {
             ultimoTickAtaque = tickActual;
+            objetivo.recibirDanio(danioAtaque);
             return danioAtaque;
         }
         return 0;
